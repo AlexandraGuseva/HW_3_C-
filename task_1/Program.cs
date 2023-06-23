696cddc926659ff4234eb9008a1bd36ade230b80
sasha <voropaeva-sasha@mail.ru>
2023-06-23 10:57:47 +0300

    Added task 1

---
+++ task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// Input
+Console.WriteLine("Please, input first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please, input second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+// Methods
+int Exponentiation(int number1, int number2)
+{
+    int result = number1;
+    for (int i = 1; i <= number2; i++)
+        {
+        result = result * i;
+        }
+        return result;
+}
+
+// Body
+int exponentation = Exponentiation(number1, number2);
+System.Console.WriteLine($"Raising the first number to the power of the second is: {exponentation}.");
