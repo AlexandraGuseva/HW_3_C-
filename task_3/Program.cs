cecf063327dfb8f53e4a1560f30e00365d6edd3b
sasha <voropaeva-sasha@mail.ru>
2023-06-24 12:25:33 +0300

    added task 3

---
+++ task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// Variables
+
+// Input
+Console.WriteLine("Please, input array length: ");
+int input_array_length = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please, input array length: ");
+int input_array_length = Convert.ToInt32(Console.ReadLine());
+// Methods
+
+// Body
